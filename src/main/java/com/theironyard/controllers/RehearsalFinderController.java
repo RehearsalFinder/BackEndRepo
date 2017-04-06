63b4c2320b1e7f092806234578f1e2e84f4d8758
jeffthebrink <jeffthebrink@gmail.com>
2017-04-06 10:31:58 -0500

    with changes

--- src/main/java/com/theironyard/controllers/RehearsalFinderController.java
+++ src/main/java/com/theironyard/controllers/RehearsalFinderController.java
@@ -1,10 +1,17 @@
 package com.theironyard.controllers;
 
+import com.fasterxml.jackson.core.ObjectCodec;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.theironyard.entities.User;
 //import com.theironyard.services.RehearsalSpaceRepository;
 import com.theironyard.services.UserRepository;
 import com.theironyard.utilities.PasswordStorage;
+import jdk.nashorn.internal.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jackson.JsonObjectDeserializer;
+import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.boot.json.JsonParser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,9 +22,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.io.IOException;
+import java.util.*;
 
 @RestController
 public class RehearsalFinderController {
@@ -41,13 +47,12 @@ public class RehearsalFinderController {
 
     @RequestMapping(path = "/add-user", method = RequestMethod.POST)
     public String signUp(@RequestBody String body, HttpServletResponse response)
-            throws PasswordStorage.CannotPerformOperationException {
-//        User user = new User(firstName, lastName, password, email, birthday);
-//        users.save(user);
+            throws PasswordStorage.CannotPerformOperationException, IOException {
         response.setContentType("application/json");
         response.setStatus(211);
+        // parse through new user data and add to users table in db
+
         System.out.println(body);
-        JsonParser p;
 
         return "{\"this\":\"should be json\"}";
     }
