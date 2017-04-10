ad5bad731fa7e523f27ad1823e1add37d803addc
jeffthebrink <jeffthebrink@gmail.com>
2017-04-10 16:27:52 -0500

    new parser working on all user routes

--- src/main/java/com/theironyard/controllers/UsersController.java
+++ src/main/java/com/theironyard/controllers/UsersController.java
@@ -7,6 +7,7 @@ import com.theironyard.serializers.RootSerializer;
 import com.theironyard.serializers.UserSerializer;
 import com.theironyard.services.UserRepository;
 import com.theironyard.utilities.JsonUser;
+import com.theironyard.utilities.PasswordStorage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletResponse;
@@ -37,7 +38,7 @@ public class UsersController {
     }
 
     @RequestMapping(path = "/login", method = RequestMethod.POST)
-    public Map<String, Object> login(@RequestBody RootParser<JsonUser> parser, HttpServletResponse response, HttpSession session)
+    public Map<String, Object> login(@RequestBody RootParser<JsonUser> parser, HttpServletResponse response)
             throws Exception {
         JsonUser jsonUser = parser.getData().getEntity();
         String password = jsonUser.getPassword();
@@ -54,22 +55,22 @@ public class UsersController {
 
     @RequestMapping(path = "/users/{id}", method = RequestMethod.GET)
     public Map<String, Object> getUser(@PathVariable("id") String id) throws Exception {
-        RootSerializer rootSerializer = new RootSerializer();
-        UserSerializer userSerializer = new UserSerializer();
         User user = users.findFirstById(id);
         return rootSerializer.serializeOne("/users/{id}", user, userSerializer);
     }
 
     @RequestMapping(path = "/delete-user", method = RequestMethod.DELETE)
-    public String deleteUser(@RequestBody String body, HttpServletResponse response) throws IOException {
-        response.setContentType("application/json");
-        ObjectMapper mapper = new ObjectMapper();
-        User selectedUser = mapper.readValue(body, User.class);
-        String email = selectedUser.getEmail();
-        User deleteUser = users.findFirstByEmail(email);
-        users.delete(deleteUser);
-        response.setStatus(201);
-        return "User removed";
+    public Map<String, Object> deleteUser(@RequestBody RootParser<JsonUser> parser, HttpServletResponse response) throws Exception {
+        JsonUser jsonUser = parser.getData().getEntity();
+        String email = jsonUser.getEmail();
+        String password = jsonUser.getPassword();
+        User user = users.findFirstByEmail(email);
+        if (users.findFirstByEmail(email) != null){
+            if (user.verifyPassword(password)) {
+                users.delete(user);
+            } else throw new Exception("Wrong credentials!");
+        }
+        return rootSerializer.serializeOne("/login", user, userSerializer);
     }
 
 
